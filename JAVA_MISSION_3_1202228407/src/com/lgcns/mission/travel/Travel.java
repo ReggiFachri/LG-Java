ec5f3259a31563e7216a21f1632e550836493e07
Grexgi <rfachri.exe@gmail.com>
2024-07-10 20:43:40 +0700

    debug{}

--- JAVA_MISSION_3_1202228407/src/com/lgcns/mission/travel/Travel.java
+++ JAVA_MISSION_3_1202228407/src/com/lgcns/mission/travel/Travel.java
@@ -38,16 +38,18 @@ public class Travel {
 	public void setResvPeople(int resvPeople) {
 		if(resvPeople < 0) {
 			System.out.println("[Error] Jumlah maksimal peserta tidak boleh lebih kecil dari 0.");					
-		} else {
+		} else	{		
 			if(resvPeople <= getMaxPeople()) {			
+
 				this.resvPeople = resvPeople;
-				System.out.println("Jumlah maksimum orang dalam reservasi sudah diubah menjadi " + getResvPeople() + " orang.");
-			}
-			else if(resvPeople > getMaxPeople()) {
-				System.out.println("[Error] Angka yang dimasukkan tidak boleh lebih besar dari jumlah maksimal reservasi ("+getMaxPeople()+" orang).");			
-			}
-			else {			
-				System.out.println("Jumlah maksimum orang tidak berhasil diubah.");			
+				System.out.println(
+						"Jumlah maksimum orang dalam reservasi sudah diubah menjadi " + getResvPeople() + " orang.");
+			} else if (resvPeople > getMaxPeople()) {
+				System.out.println(
+						"[Error] Angka yang dimasukkan tidak boleh lebih besar dari jumlah maksimal reservasi ("
+								+ getMaxPeople() + " orang).");
+			} else {
+				System.out.println("Jumlah maksimum orang tidak berhasil diubah.");
 			}
 		}
 	}
@@ -58,10 +60,12 @@ public class Travel {
 		} else {
 			if (maxPeople > this.maxPeople) {
 				this.maxPeople = maxPeople;
-				System.out.println("Jumlah maksimum orang dalam reservasi sudah diubah menjadi " + this.maxPeople + " orang.");
+				System.out.println(
+						"Jumlah maksimum orang dalam reservasi sudah diubah menjadi " + this.maxPeople + " orang.");
 			} else if (maxPeople < this.resvPeople) {
 				this.maxPeople = this.resvPeople;
-				System.out.println("[Info] Dikarenakan jumlah reservasi yang sudah ada, maka jumlah maksimal orang akan disesuaikan dengan jumlah reservasi saat ini.");
+				System.out.println(
+						"[Info] Dikarenakan jumlah reservasi yang sudah ada, maka jumlah maksimal orang akan disesuaikan dengan jumlah reservasi saat ini.");
 				System.out.println("Jumlah maksimal reservasi diubah menjadi " + this.resvPeople + " orang.");
 			} else {
 				System.out.println("Jumlah maksimum orang tidak berhasil diubah.");
@@ -71,23 +75,25 @@ public class Travel {
 
 	public void printTravelInfo() {
 		String type = null;
-		if(getTravelType() == 1) {
+		if (getTravelType() == 1) {
 			type = "Paket";
-		}else if(getTravelType() == 0) {
+		} else if (getTravelType() == 0) {
 			type = "Individu";
 		}
 
 		String status = null;
-		if(getMaxPeople() == getResvPeople()) {
+		if (getMaxPeople() == getResvPeople()) {
 			status = "Sold out";
-		}else {
+		} else {
 			status = "-";
 		}
 
-		//		System.out.println( this.travelCode+"\t"+this.cityName+"\t"+this.flight+"\t"+type+"\t"+this.maxPeople+"\t"+this.resvPeople+"\t"+status);
+		// System.out.println(
+		// this.travelCode+"\t"+this.cityName+"\t"+this.flight+"\t"+type+"\t"+this.maxPeople+"\t"+this.resvPeople+"\t"+status);
 
 		String format = "%-15s\t%-15s\t%-20s\t%-15s\t%-20s\t%-25s\t%-15s\n";
-		System.out.printf(format, this.travelCode, this.cityName, this.flight, type, this.maxPeople, this.resvPeople, status);
+		System.out.printf(format, this.travelCode, this.cityName, this.flight, type, this.maxPeople, this.resvPeople,
+				status);
 	}
 
 }
--- JAVA_MISSION_3_1202228407/src/com/lgcns/mission/travel/TravelTest.java
+++ JAVA_MISSION_3_1202228407/src/com/lgcns/mission/travel/TravelTest.java
@@ -5,14 +5,14 @@ package com.lgcns.mission.travel;
 public class TravelTest {
 	public static void main(String[] args) {
 
-		//Scanner s = new Scanner(System.in);
+		// Scanner s = new Scanner(System.in);
 
 		Travel[] travels = {
-				new Travel( "TRV001", "Munchen", "German Airways", Travel.INDIVIDUAL, 10 ),
-				new Travel( "TRV002", "Prague", "Air France", Travel.INDIVIDUAL, 20 ),
-				new Travel( "TRV003", "LA", "Delta Airlines", Travel.PACKAGE, 12 ),
-				new Travel( "TRV004", "Osaka", "Korean Air", Travel.INDIVIDUAL, 15 ),
-				new Travel( "TRV005", "Shanghai", "Shouthern Airlines", Travel.PACKAGE, 10 )
+				new Travel("TRV001", "Munchen", "German Airways", Travel.INDIVIDUAL, 10),
+				new Travel("TRV002", "Prague", "Air France", Travel.INDIVIDUAL, 20),
+				new Travel("TRV003", "LA", "Delta Airlines", Travel.PACKAGE, 12),
+				new Travel("TRV004", "Osaka", "Korean Air", Travel.INDIVIDUAL, 15),
+				new Travel("TRV005", "Shanghai", "Shouthern Airlines", Travel.PACKAGE, 10)
 		};
 
 		int menu = -1;
@@ -21,107 +21,110 @@ public class TravelTest {
 		int resvPeople = 0;
 		boolean travelFound = false;
 
-		while(menu != 9) {
+		while (menu != 9) {
 			printMenu();
 			menu = MissionUtil.getUserIntegerInput();
 
-			switch(menu) {
-			case 1:
-				printHeader();
-				for(Travel travel : travels) {
-					travel.printTravelInfo();
-				}
-				break;
-			case 2:
-				printHeader();
-				for(Travel travel : travels) {
-					if(travel.getTravelType() == 0) {						
+			switch (menu) {
+				case 1:
+					printHeader();
+					for (Travel travel : travels) {
 						travel.printTravelInfo();
 					}
-				}
-				break;
-			case 3:
-				printHeader();
-				for(Travel travel : travels) {
-					if(travel.getTravelType() == 1) {						
-						travel.printTravelInfo();
+					break;
+				case 2:
+					printHeader();
+					for (Travel travel : travels) {
+						if (travel.getTravelType() == 0) {
+							travel.printTravelInfo();
+						}
 					}
-				}
-
-				break;
-
-			case 4:
-				System.out.print("Masukkan code travel : ");
-				travelCode = MissionUtil.getCodeInput();
-				System.out.print("Masukkan jumlah maksimum orang dalam reservasi untuk diubah : ");
-				maxPeople = MissionUtil.getUserIntegerInput();
-
-				for(Travel travel : travels) {
-					if(travel.getTravelCode().equals(travelCode)) {						
-						travel.setMaxPeople(maxPeople);
-						travelFound = true;
-						break;
+					break;
+				case 3:
+					printHeader();
+					for (Travel travel : travels) {
+						if (travel.getTravelType() == 1) {
+							travel.printTravelInfo();
+						}
 					}
-				}
 
-				if(!travelFound) {
-					System.out.println("[Error] Tidak ditemukan paket yang cocok dengan code travel ["+travelCode+"].");
-				}
+					break;
 
-				break;
+				case 4:
+					System.out.print("Masukkan code travel : ");
+					travelCode = MissionUtil.getCodeInput();
+					System.out.print("Masukkan jumlah maksimum orang dalam reservasi untuk diubah : ");
+					maxPeople = MissionUtil.getUserIntegerInput();
 
-			case 5:
-				System.out.print("Masukkan code travel : ");
-				travelCode = MissionUtil.getCodeInput();
-				System.out.print("Masukkan jumlah maksimum orang dalam reservasi untuk diubah : ");
-				resvPeople = MissionUtil.getUserIntegerInput();
-
-				for(Travel travel : travels) {
-					if(travel.getTravelCode().equals(travelCode)) {						
-						travel.setResvPeople(resvPeople);
-						travelFound = true;
-						break;
+					for (Travel travel : travels) {
+						if (travel.getTravelCode().equals(travelCode)) {
+							travel.setMaxPeople(maxPeople);
+							travelFound = true;
+							break;
+						}
 					}
-				}
 
-				if(!travelFound) {
-					System.out.println("[Error] Tidak ditemukan paket yang cocok dengan code travel ["+travelCode+"].");
-				}
+					if (!travelFound) {
+						System.out.println(
+								"[Error] Tidak ditemukan paket yang cocok dengan code travel [" + travelCode + "].");
+					}
 
-				break;
+					break;
 
-			case 9:
-				System.out.println("Keluar!!");					
-				break;
+				case 5:
+					System.out.print("Masukkan code travel : ");
+					travelCode = MissionUtil.getCodeInput();
+					System.out.print("Masukkan jumlah maksimum orang dalam reservasi untuk diubah : ");
+					resvPeople = MissionUtil.getUserIntegerInput();
 
-			default:
-				System.out.println("Silahkan masukkan ulang!!");
+					for (Travel travel : travels) {
+						if (travel.getTravelCode().equals(travelCode)) {
+							travel.setResvPeople(resvPeople);
+							travelFound = true;
+							break;
+						}
+					}
+
+					if (!travelFound) {
+						System.out.println(
+								"[Error] Tidak ditemukan paket yang cocok dengan code travel [" + travelCode + "].");
+					}
+
+					break;
+
+				case 9:
+					System.out.println("Keluar!!");
+					break;
+
+				default:
+					System.out.println("Silahkan masukkan ulang!!");
 			}
 
 		}
 	}
 
 	public static void printMenu() {
-		System.out.println( "========== < Menu > ==========" );
-		System.out.println( " 1. Lihat semua paket perjalanan" );
-		System.out.println( " 2. Pencarian paket individu" );
-		System.out.println( " 3. Pencarian paket perjalanan" );
-		System.out.println( " 4. Perubahan jumlah maksimal peserta" );
-		System.out.println( " 5. Perubahan jumlah peserta dalam reservasi" );
-		System.out.println( " 9. End" );
-		//System.out.println( "==============================" );
+		System.out.println("========== < Menu > ==========");
+		System.out.println(" 1. Lihat semua paket perjalanan");
+		System.out.println(" 2. Pencarian paket individu");
+		System.out.println(" 3. Pencarian paket perjalanan");
+		System.out.println(" 4. Perubahan jumlah maksimal peserta");
+		System.out.println(" 5. Perubahan jumlah peserta dalam reservasi");
+		System.out.println(" 9. End");
+		// System.out.println( "==============================" );
 		printSeperator();
-		System.out.print( "Pilih Menu : " );
+		System.out.print("Pilih Menu : ");
 	}
 
 	public static void printHeader() {
 		printSeperator();
-		System.out.println("Kode Travel\tNama Kota\tAirline\t\t\tJenis Paket\tJumlah Peserta Max\tJml Peserta Saat Ini");
+		System.out
+				.println("Kode Travel\tNama Kota\tAirline\t\t\tJenis Paket\tJumlah Peserta Max\tJml Peserta Saat Ini");
 		printSeperator();
 	}
 
 	public static void printSeperator() {
-		System.out.println( "===============================" );
+		System.out.println("===============================");
 	}
 
 }
