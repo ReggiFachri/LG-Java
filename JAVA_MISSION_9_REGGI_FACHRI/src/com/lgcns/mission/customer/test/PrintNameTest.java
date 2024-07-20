da38a191e79b185710a392f1dee162486910175e
Grexgi <rfachri.exe@gmail.com>
2024-07-20 19:50:48 +0700

    init practice 11

---
+++ JAVA_MISSION_9_REGGI_FACHRI/src/com/lgcns/mission/customer/test/PrintNameTest.java
@@ -0,0 +1,19 @@
+package com.lgcns.mission.customer.test;
+
+public class PrintNameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"Jeon Woo-chi", "Hong Gil-dong", "Hong Gil-sun", "Lim Kkeok-jeong"};
+		
+		for ( int inx = 1; inx <= names.length; inx++ ) {
+			try {
+                System.out.println( names[inx] );
+            } catch ( ArrayIndexOutOfBoundsException e ) {
+                System.out.println(
+                   "Error! Anda mengakses jalan yang salah!");
+            }}
+		
+	}
+
+}
